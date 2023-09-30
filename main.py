2b936d046e2cf19b90eb5415f74c53b186ef7bab
Wyckoff, Dylan C <dwyckoff3@gatech.edu>
2023-09-30 11:32:23 -0400

    Basics

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+from secret import application_id, bot_token
+from config import config
+
+
+class MyBot(commands.Bot):
+    async def setup_hook(self):
+        for ext in config["cogs"]:
+            await self.load_extension(ext)
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+        print(f"{self.user} has connected to Discord!")
+
+        
+bot = MyBot(
+    command_prefix=config["command_prefix"],
+    intents=discord.Intents.all(),
+    application_id=application_id,
+)
+
+bot.run(bot_token)
